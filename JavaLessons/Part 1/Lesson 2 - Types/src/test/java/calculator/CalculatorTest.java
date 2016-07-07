3dc6e90b6f0c7973b3f62b95dc0a60fc6255ec16
lexa-vic <kostikovav1989@gmail.com>
2016-07-07 22:19:14 +0300

    [MOD] Сделал проект модульным, поправил тесты

---
+++ JavaLessons/Part 1/Lesson 2 - Types/src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,67 @@
+package calculator;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Алексей on 06.07.2016.
+ */
+public class CalculatorTest {
+    /** Ссылка на тестируемый класс калькулятора */
+    private Calculator calc;
+    /** Результат */
+    private double result;
+
+    /** Инициализация перед тестами */
+    @Before
+    public void CreateCalc(){
+        calc = new Calculator();
+    }
+
+    /** Тест сложения */
+    @Test
+    public void add() throws Exception {
+        calc.add(3, 2);
+
+        result = calc.getResult();
+        assertThat(5.0, is(result));
+    }
+
+    /** Тест вычитания */
+    @Test
+    public void subtract() throws Exception {
+        Calculator calc = new Calculator();
+
+        calc.subtract(2, 3);
+
+        result = calc.getResult();
+        assertThat(-1.0, is(result));
+    }
+
+    /** Тест деления */
+    @Test
+    public void div() throws Exception {
+        Calculator calc = new Calculator();
+
+        calc.div(10, 2);
+
+        result = calc.getResult();
+        assertThat(5.0, is(result));
+    }
+
+    /** Тест умножения */
+    @Test
+    public void mult() throws Exception {
+        Calculator calc = new Calculator();
+
+        calc.mult(6, 6);
+
+        result = calc.getResult();
+        assertThat(36.0, is(result));
+    }
+
+}
