77c35b87688f59e9848b62a9585331f5f4934991
lexa-vic <kostikovav1989@gmail.com>
2016-07-12 15:25:05 +0300

    [ADD] Добавил новое задание со строками и тесты к нему.

---
+++ JavaLessons/Part 1/Lesson 6 - Final task/src/main/java/strings/StringFind.java
@@ -0,0 +1,18 @@
+package strings;
+
+/**
+ * Created by Алексей on 12.07.2016.
+ */
+public class StringFind {
+
+    /**
+     * Проверка, является ли sub подстрокой origin
+     * @param origin Главная срока
+     * @param sub    Подстрока
+     * @return true - sub является подстрокой origin, false - не является
+     */
+    boolean contains(String origin, String sub){
+
+        return false;
+    }
+}
---
+++ JavaLessons/Part 1/Lesson 6 - Final task/src/test/java/strings/StringFindTest.java
@@ -0,0 +1,46 @@
+package strings;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Алексей on 12.07.2016.
+ */
+public class StringFindTest {
+    /**
+     * Проверка вхождения подстроки в строку
+     * @throws Exception
+     */
+    @Test
+    public void containsTrue() throws Exception {
+        boolean result = false;
+        String origin  = "ab bc cd de ef";
+        String sub     = "de";
+
+        StringFind stringSub = new StringFind();
+
+        result = stringSub.contains(origin, sub);
+
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Проверка вхождения подстроки в строку
+     * @throws Exception
+     */
+    @Test
+    public void containsFalse() throws Exception {
+        boolean result = false;
+        String origin  = "ab bc cd de ef";
+        String sub     = "xd";
+
+        StringFind stringSub = new StringFind();
+
+        result = stringSub.contains(origin, sub);
+
+        assertThat(result, is(false));
+    }
+
+}
