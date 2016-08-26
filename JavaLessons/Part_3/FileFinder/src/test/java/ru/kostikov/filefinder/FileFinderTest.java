705e2f6f8413e8951c5fa884f90234ae36cdd6f9
lexa-vic <lexa-vic@mail.ru>
2016-08-26 20:48:10 +0300

    [MOD] Подчистил тест, доработал сборку.

--- JavaLessons/Part_3/FileFinder/src/test/java/ru/kostikov/filefinder/FileFinderTest.java
+++ JavaLessons/Part_3/FileFinder/src/test/java/ru/kostikov/filefinder/FileFinderTest.java
@@ -1,5 +1,6 @@
 package ru.kostikov.filefinder;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -18,17 +19,22 @@ import static org.junit.Assert.*;
  * Created by Алексей on 23.08.2016.
  */
 public class FileFinderTest {
+    private String exepect;
+    private Path startPath;
+
+    @Before
+    public void FileFinderTestInit(){
+        ClassLoader loader = FileFinderTest.class.getClassLoader();
+        this.exepect = loader.getResource("ru/kostikov/filefinder/FileFinder.class").getPath();
+        this.exepect = this.exepect.substring(1, exepect.length()).replace('/', '\\');
+        this.startPath = Paths.get("").toAbsolutePath();
+    }
+
     @Test
     public void findFilesByMask() throws Exception {
-        ClassLoader loader = FileFinderTest.class.getClassLoader();
-        String exepect = loader.getResource("ru/kostikov/filefinder/FileFinder.class").getPath();
         String result = null;
 
-        exepect = exepect.substring(1, exepect.length()).replace('/', '\\');
-
-        Path startPath = Paths.get("").toAbsolutePath();
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
         // Создаем класс, начинаем искать
         FileFinder fileFinder = new FileFinder(new GlobMacther("*Finder.class"), baos);
         fileFinder.findFiles(startPath);
@@ -46,15 +52,10 @@ public class FileFinderTest {
 
     @Test
     public void findFilesByName() throws Exception {
-        ClassLoader loader = FileFinderTest.class.getClassLoader();
-        String exepect = loader.getResource("ru/kostikov/filefinder/FileFinder.class").getPath();
         String result = null;
-
-        exepect = exepect.substring(1, exepect.length()).replace('/', '\\');
 
         Path startPath = Paths.get("").toAbsolutePath();
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
         // Создаем класс, начинаем искать
         FileFinder fileFinder = new FileFinder(new FullNameMatcher("FileFinder.class"), baos);
         fileFinder.findFiles(startPath);
@@ -66,23 +67,15 @@ public class FileFinderTest {
                 break;
             }
         }
-
         assertThat(exepect,  is(result));
     }
 
     @Test
     public void findFilesByRegExp() throws Exception {
-        ClassLoader loader = FileFinderTest.class.getClassLoader();
-        String exepect = loader.getResource("ru/kostikov/filefinder/FileFinder.class").getPath();
         String result = null;
 
-        exepect = exepect.substring(1, exepect.length()).replace('/', '\\');
-
-        Path startPath = Paths.get("").toAbsolutePath();
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
         PathMatcher matcher = FileSystems.getDefault().getPathMatcher("regex:.+\\.(class)");
-
         // Создаем класс, начинаем искать
         FileFinder fileFinder = new FileFinder(new RegExpMatcher(Pattern.compile("FileFinder.class")), baos);
         fileFinder.findFiles(startPath);
@@ -94,7 +87,6 @@ public class FileFinderTest {
                 break;
             }
         }
-
         assertThat(exepect,  is(result));
     }
 
