2024e94392ae056624545cb4f4abe81f24defa66
Костиков Алексей <kostikov@starline.ru>
2016-10-03 10:20:16 +0300

    [MOD] Добавил генерацию линий для сравнения с шаблонами.

--- JavaLessons/Part_4/TicTacToe/src/main/java/ru/kostikov/ai/ComputerPlayer.java
+++ JavaLessons/Part_4/TicTacToe/src/main/java/ru/kostikov/ai/ComputerPlayer.java
@@ -27,7 +27,16 @@ public class ComputerPlayer implements Player {
     @Override
     public Move getMove() {
 
-        this.field.getMoves();
+        for (Move move: this.field.getMoves()){
+            String [] lines = getLines(this.field.field, this.field.winLength, move);
+
+            for (String line : lines ){
+                if (line != null && line.equals("")){
+
+                }
+            }
+
+        }
         return null;
     }
 
@@ -47,5 +56,71 @@ public class ComputerPlayer implements Player {
     @Override
     public void badMove() {
 
+    }
+
+    private String[] getLines(int[][] field, int winLength, Move move){
+        String[] lines = new String[4];
+        StringBuilder line = new StringBuilder();
+        int x = move.getRow();
+        int y = move.getCol();
+
+        // Собираем линию по вертикали (|)
+        for (int i = x - winLength; i <= x + winLength; i++){
+            if (i >= 0 && i < field.length){
+                if(field[i][y] == 1){
+                    line.append("x");
+                } else if (field[i][y] == 0) {
+                    line.append("0");
+                }
+            }
+        }
+        lines[0] = line.toString();
+
+        line = new StringBuilder();
+        // Собираем линию по горизонтали(-)
+        for (int i = y - winLength; i <= y + winLength; i++){
+            if (i >= 0 && i < field.length){
+                if(field[x][i] == 1){
+                    line.append("x");
+                } else if (field[x][i] == 0) {
+                    line.append("0");
+                }
+            }
+        }
+        lines[1] = line.toString();
+
+        line = new StringBuilder();
+        int diag_y = y - winLength;
+        // Собираем линию по диагонали(/)
+        for (int i = x - winLength; i < x + winLength; i++){
+            diag_y +=1;
+            if (i >= 0 && i < field.length &&
+                    diag_y >= 0 && diag_y < field.length ){
+                if(field[i][diag_y] == 1){
+                    line.append("x");
+                } else if (field[i][diag_y] == 0) {
+                    line.append("0");
+                }
+            }
+        }
+        lines[2] = line.toString();
+        line = new StringBuilder();
+
+        diag_y = y + winLength;
+        // Собираем линию по диагонали(/)
+        for (int i = x - winLength; i <= x + winLength; i++){
+            diag_y -=1;
+            if (i >= 0 && i < field.length &&
+                    diag_y >= 0 && diag_y < field.length ){
+                if(field[i][diag_y] == 1){
+                    line.append("x");
+                } else if (field[i][diag_y] == 0) {
+                    line.append("0");
+                }
+            }
+        }
+        lines[3] = line.toString();
+
+        return lines;
     }
 }
