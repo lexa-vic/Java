64a6d2e98c027085063270d77f8dc1f15cc92336
lexa-vic <lexa-vic@mail.ru>
2016-10-16 19:35:46 +0300

    [ADD] Добавил класс User для тестирования MAP

---
+++ JavaLessons/Part_5/Map/src/main/java/ru/kostikov/User.java
@@ -0,0 +1,37 @@
+package ru.kostikov;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Алексей on 16.10.2016.
+ */
+public class User {
+    public String name;
+    public int children;
+    public Calendar birthday;
+
+    public User(){}
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User first = new User("Aleksey", 0, new GregorianCalendar(1989,
+                Calendar.MARCH, 06));
+        User second = new User("Aleksey", 0, new GregorianCalendar(1989,
+                Calendar.MARCH, 06));
+
+        Map<User, Object> userMap = new HashMap<>();
+
+        userMap.put(first, new Object());
+        userMap.put(second, new Object());
+
+        System.out.println(userMap);
+    }
+}
---
+++ JavaLessons/Part_5/Map/src/test/java/ru/kostikov/UserTest.java
@@ -0,0 +1,10 @@
+package ru.kostikov;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Алексей on 16.10.2016.
+ */
+public class UserTest {
+
+}
