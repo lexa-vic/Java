0f1f6a16fe51b885ef2c9bb29cbf0054056b0f7f
lexa-vic <lexa-vic@mail.ru>
2016-10-16 20:49:03 +0300

    [MOD] Переопредил метод equals

--- JavaLessons/Part_5/Map/src/main/java/ru/kostikov/User.java
+++ JavaLessons/Part_5/Map/src/main/java/ru/kostikov/User.java
@@ -35,6 +35,18 @@ public class User {
         System.out.println(userMap);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (children != user.children) return false;
+        if (!name.equals(user.name)) return false;
+        return birthday.equals(user.birthday);
+
+    }
 
     @Override
     public int hashCode() {
