a9e80ee8758342dc307654e6e1e4befe0b1551d0
lexa-vic <lexa-vic@mail.ru>
2016-10-16 20:32:29 +0300

    [MOD] Переопредилил hashCode

--- JavaLessons/Part_5/Map/src/main/java/ru/kostikov/User.java
+++ JavaLessons/Part_5/Map/src/main/java/ru/kostikov/User.java
@@ -34,4 +34,13 @@ public class User {
 
         System.out.println(userMap);
     }
+
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + children;
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+        return result;
+    }
 }
