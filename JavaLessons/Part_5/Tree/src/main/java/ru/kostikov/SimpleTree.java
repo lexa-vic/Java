e89185517232db4e656bffe2bcb4b5f86c7eb3d8
lexa-vic <lexa-vic@mail.ru>
2016-10-19 22:17:13 +0300

    [MOD] Добавил реализацию дерева.

--- JavaLessons/Part_5/Tree/src/main/java/ru/kostikov/SimpleTree.java
+++ JavaLessons/Part_5/Tree/src/main/java/ru/kostikov/SimpleTree.java
@@ -1,37 +1,144 @@
 package ru.kostikov;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.TreeMap;
 
 /**
  * Created by Алексей on 18.10.2016.
  */
-public class SimpleTree<K> {
+public class SimpleTree<K extends Comparable> {
     /**
      * Node in the Tree.  Doubles as a means to pass key-value pairs back to
      * user (see Map.Entry).
      */
-     public static class Leaf<K>{
-        K key;
-        Leaf<K> left;
-        Leaf<K> right;
-        Leaf<K> parent;
+     public static class Node<K>{
+        public K key;
+        public Node<K> left;
+        public Node<K> right;
+        public Node<K> parent;
 
-        Leaf(K key) {
+        public Node(K key, Node<K> parent) {
             this.key = key;
+            this.parent = parent;
         }
 
-        private Leaf<K> root;
+    }
+    /** Tree root */
+    private Node<K> root = null;
 
-    public void addChild(Leaf<K> leaf){
-        if (root == null){
-            root = leaf;
+    private boolean balanced = true;
+
+
+    /**
+     * Add new node
+     * @param value
+     */
+    public void addChild(K value){
+        Node<K> node = root;
+
+        if (node == null){
+            root = new Node<K>(value, null);
         } else {
+            int cmp;
+            Node<K> parent = null;
+            Comparable<? super K> k = (Comparable<? super K>) value;
 
+            do {
+                parent = node;
+                cmp = k.compareTo(node.key);
+
+                if (cmp < 0) {
+                    node = node.left;
+                } else if (cmp > 0){
+                    node = node.right;
+                } else {
+                    // Item already exist
+                    return;
+                }
+            }while(node != null);
+
+            Node<K> newItem = new Node<K>(value, parent);
+            if (cmp < 0)
+                parent.left = newItem;
+            else
+                parent.right = newItem;
         }
     }
+
+    /**
+     *  Gets all tree values
+     * @return
+     */
+    public List<K> getChildren() {
+        List<K> list = new ArrayList<K>();
+        Node<K> p = root;
+
+        if (p != null){
+            getChildrenNodes(p, list);
+        }
+        return list;
     }
-    List<K> getChildren() {
+
+    /**
+     * Finds node
+     * @param value
+     * @return
+     */
+    public Node<K> getNode(K value){
+        Node<K> p = root;
+        Comparable<? super K> k = (Comparable<? super K>) value;
+        while (p != null) {
+            int cmp = k.compareTo(p.key);
+            if (cmp < 0)
+                p = p.left;
+            else if (cmp > 0)
+                p = p.right;
+            else
+                return p;
+        }
         return null;
+    }
+
+    /**
+     * Checks tree balance
+     * @return true if tree is balanced
+     */
+    public boolean isBalancedTree(){
+        balanceCheck(root);
+        return this.balanced;
+    }
+
+    private void balanceCheck(Node<K> node){
+        int branchCnt = 0;
+        if (node.left != null){
+            balanceCheck(node.left);
+            branchCnt++;
+        }
+        if (node.right != null){
+            balanceCheck(node.right);
+            branchCnt++;
+        }
+        if ((branchCnt % 2) != 0){
+            this.balanced = false;
+        }
+
+    }
+
+    /**
+     * Collect all values from nodes
+     * @param node
+     * @param valueArray
+     */
+    private void getChildrenNodes(Node<K> node, List<K> valueArray){
+        valueArray.add(node.key);
+        if (node.left != null){
+            getChildrenNodes(node.left, valueArray);
+        }
+
+        if (node.right != null){
+            getChildrenNodes(node.right, valueArray);
+        }
     }
 }
--- JavaLessons/Part_5/Tree/src/test/java/ru/kostikov/SimpleTreeTest.java
+++ JavaLessons/Part_5/Tree/src/test/java/ru/kostikov/SimpleTreeTest.java
@@ -1,10 +1,102 @@
 package ru.kostikov;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 /**
  * Created by Алексей on 18.10.2016.
  */
 public class SimpleTreeTest {
+
+    @Test
+    public void whenAddAneNodeWhenGetThisNode(){
+        SimpleTree<Integer> tree = new SimpleTree<>();
+        List<Integer> nodes = new ArrayList<>();
+        Integer[] expected = new Integer[]{5, 4, 3, 6, 7};
+
+        tree.addChild(5);
+        tree.addChild(4);
+        tree.addChild(6);
+        tree.addChild(3);
+        tree.addChild(7);
+
+        nodes = tree.getChildren();
+
+        Assert.assertThat(expected, is(nodes.toArray()));
+    }
+
+    @Test
+    public void whenFindNodeThenGetThisNode(){
+        SimpleTree<Integer> tree = new SimpleTree<>();
+        List<Integer> nodes = new ArrayList<>();
+        Integer expected = 7;
+
+        tree.addChild(5);
+        tree.addChild(4);
+        tree.addChild(6);
+        tree.addChild(3);
+        tree.addChild(7);
+
+        SimpleTree.Node<Integer>  node = tree.getNode(7);
+
+        Assert.assertThat(expected, is(node.key));
+    }
+
+    @Test
+    public void whenFindNodeThenNotFindNode(){
+        SimpleTree<Integer> tree = new SimpleTree<>();
+        List<Integer> nodes = new ArrayList<>();
+        Integer expected = 7;
+
+        tree.addChild(5);
+        tree.addChild(4);
+        tree.addChild(6);
+        tree.addChild(3);
+        tree.addChild(7);
+
+        SimpleTree.Node<Integer>  node = tree.getNode(8);
+
+        Assert.assertThat(null, is(node));
+    }
+
+    @Test
+    public void whenTreeIsBalancedThenReturnTrue(){
+        SimpleTree<Integer> tree = new SimpleTree<>();
+        List<Integer> nodes = new ArrayList<>();
+        Integer expected = 7;
+
+        tree.addChild(15);
+        tree.addChild(7);
+        tree.addChild(35);
+        tree.addChild(1);
+        tree.addChild(9);
+        tree.addChild(20);
+        tree.addChild(40);
+
+        Assert.assertThat(true, is(tree.isBalancedTree()));
+    }
+
+    @Test
+    public void whenTreeIsNotBalancedThenReturnFalse(){
+        SimpleTree<Integer> tree = new SimpleTree<>();
+        List<Integer> nodes = new ArrayList<>();
+        Integer expected = 7;
+
+        tree.addChild(5);
+        tree.addChild(4);
+        tree.addChild(6);
+        tree.addChild(3);
+        tree.addChild(7);
+
+        Assert.assertThat(false, is(tree.isBalancedTree()));
+    }
+
+
 
 }
