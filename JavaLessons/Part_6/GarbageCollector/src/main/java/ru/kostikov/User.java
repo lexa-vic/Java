8fc40b381dd5cea981280060624043c2cc1c2008
lexa-vic <lexa-vic@mail.ru>
2016-10-27 14:19:53 +0300

    [ADD] Первое задания по GarbageCollector

---
+++ JavaLessons/Part_6/GarbageCollector/src/main/java/ru/kostikov/User.java
@@ -0,0 +1,46 @@
+package ru.kostikov;
+
+/**
+ * Created by Алексей on 25.10.2016.
+ */
+public class User {
+
+    private String name;
+    private Integer age;
+    private int id;
+
+
+    public User(int id, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println(String.format("User %s is finalize",this));
+
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", age=" + age;
+    }
+
+    public static void main(String[] args) {
+        for (int index = 0; index < 10000; index++){
+         System.out.println(new User(index, String.valueOf(index),index));
+        }
+        int mb = 1024 * 1024;
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println("##### Heap utilization statistics [MB] #####");
+        System.out.println("Used memory: "+ (runtime.totalMemory() - runtime.freeMemory())/mb + "Mb");
+        System.out.println("Free memory: "+ (runtime.freeMemory())/mb + "Mb");
+        System.out.println("Total memory: "+ (runtime.totalMemory() )/mb + "Mb");
+        System.out.println("Max memory: "+ (runtime.maxMemory() )/mb + "Mb");
+
+    }
+}
